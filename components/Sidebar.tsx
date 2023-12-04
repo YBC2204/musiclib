970d57b2147d2ae3bead57cd9bd3d3a7b103194d
Yadhukrishnan <ybc2204@gmail.com>
2023-12-04 20:53:24 +0530

    initial

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,10 +1,40 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
+import { HiHome } from "react-icons/hi"
+import { BiSearch } from "react-icons/bi"
+import Box from "./Box";
 interface SidebarProps{
     children: React.ReactNode;
 }
 
 const Sidebar: React.FC<SidebarProps> = ({children}) => {
+
+  const pathName = usePathname();
+  
+  const routes = useMemo(()=>[
+    {
+      icon: HiHome,
+      label:'Home',
+      active: pathName !== '/search',
+      href:'/',
+    },
+    {
+      icon:BiSearch,
+      label:'Search',
+      active: pathName=== '/search',
+      href:'/search'
+    }
+  ],[])
   return (
-    <div>{children}</div>
+    <div className="flex h-full">
+      <div className="hidden md:flex flex-col gap-y-2 bg-black h-full w-[300px] p-2">
+         <Box>
+          {children}
+         </Box>
+      </div>
+    </div>
   )
 }
 
