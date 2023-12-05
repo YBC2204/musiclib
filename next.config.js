8cbde1081a1e094cba04da451ea128e4fd489465
Yadhukrishnan <ybc2204@gmail.com>
2023-12-06 01:32:52 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains:[
+            "tpvtwljqyimrfodkkgsv.supabase.co"
+    ]
+    }
+}
 
 module.exports = nextConfig
