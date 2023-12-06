320a24b182796c46d1a1c5fcc76f63493dfe88e0
Anjana Kj <kjanjana0081@gmail.com>
2023-12-06 16:34:06 +0530

    feat: like button colour changed

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -92,7 +92,7 @@ const LikeButton: React.FC<LikeButtonProps> = ({
       "
       onClick={handleLike}
     >
-      <Icon color={isLiked ? '#22c55e' : 'white'} size={25} />
+      <Icon color={isLiked ? '#1E90FF' : 'white'} size={25} />
     </button>
   );
 }
